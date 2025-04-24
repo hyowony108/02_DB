1b11a968fa14b2b29854e72d9dbe2e83db8e4f08
hyowony108 <hyowony108@gmail.com>
2025-04-24 12:05:37 +0900

    사용자 계정

--- 사용자계정script.sql
+++ 사용자계정script.sql
@@ -11,14 +11,14 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 -- 계정 생성 구문 (username : kh / password : kh1234)
-CREATE USER todo_jdbc IDENTIFIED BY todoList1234;
+CREATE USER boardProject IDENTIFIED BY boardProject1234;
 
-GRANT RESOURCE, CONNECT TO todo_jdbc;
+GRANT RESOURCE, CONNECT TO boardProject;
 -- 사용자 계정 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
 -- CONNECT : DB에 연결하고 로그인 할 수 있는 권한
 
-ALTER USER todo_jdbc DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
+ALTER USER boardProject DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
 
 
@@ -58,6 +58,9 @@ GRANT RESOURCE, CONNECT TO kh;
 
 ALTER USER kh DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
 
+-----------------------------------------------------------------------------
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 
 CREATE USER workbook IDENTIFIED BY workbook ;
 
@@ -65,4 +68,22 @@ GRANT RESOURCE, CONNECT TO workbook;
 
 ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
 
+-----------------------------------------------------------------------------
+
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+CREATE USER todoList_boot IDENTIFIED BY todoList1234;
+
+GRANT RESOURCE, CONNECT TO todoList_boot;
+
+ALTER USER todoList_boot DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+-----------------------------------------------------------------------------
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+CREATE USER boardProject IDENTIFIED BY boardProject1234;
+
+GRANT RESOURCE, CONNECT TO boardProject;
+
+ALTER USER boardProject DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM ;
 
